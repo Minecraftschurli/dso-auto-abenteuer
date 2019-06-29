0f35c31ec36f8720774f88a939d633130d95545f
Minecraftschurli <minecraftschurli@gmail.com>
2019-06-29 20:06:46 +0200

    finish setup

--- start.py
+++ start.py
@@ -1,10 +1,12 @@
+from time import sleep
+
 import cv2
 import numpy
 from PIL import ImageChops, ImageGrab  # $ pip install pillow
 from pynput.mouse import Listener as MouseListener
 
 
-def click():
+def click(_id=''):
     global pos
     pos = None
 
@@ -16,12 +18,13 @@ def click():
     mouse_listener.start()
     while mouse_listener.running:
         pass
+    print(f'Pos{ _id }: { pos }')
     return pos
 
 
 def setup():
     global notif_bbox
-    notif_bbox = click() + click()
+    notif_bbox = click('1') + click('2')
 
 
 if __name__ == '__main__':
@@ -34,6 +37,7 @@ if __name__ == '__main__':
         bbox = diff.getbbox()
         if bbox is not None:  # exact comparison
             break
+        sleep(1)
 
     print(f"bounding box of non-zero difference: {(bbox,)}")
     # superimpose the inverted image and the difference
